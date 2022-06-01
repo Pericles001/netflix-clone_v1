41089fe659dc6d4a131bd3845dabc920341bf90d
pericles001 <periclesadjovi@outlook.fr>
2022-06-01 14:39:43 +0100

    Fetching url from movies categories

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Row from "./Row";
+import Requests from "./requests";
 import './App.css';
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
       <h1>Hi clever programmer , let's build netflix clone today</h1>
-        <Row title="NETFLIX ORIGINALS"/>
-        <Row title="Trending now"/>
+        <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+        <Row title="Trending now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
