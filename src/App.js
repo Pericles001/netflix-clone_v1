1623547de2fe7db58d1eb05966b1470ddb06ea32
pericles001 <periclesadjovi@outlook.fr>
2022-06-01 14:32:49 +0100

    Working on row container to display movies catalogs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Row from "./Row";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi clever programmer , let's build netflix clone today</h1>
+        <Row title="NETFLIX ORIGINALS"/>
+        <Row title="Trending now"/>
     </div>
   );
 }
