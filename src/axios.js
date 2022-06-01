9fe9225489fffc9ca5c37260426493ee7a3ceb24
pericles001 <periclesadjovi@outlook.fr>
2022-06-01 14:55:36 +0100

    Fix typo

--- src/axios.js
+++ src/axios.js
@@ -4,6 +4,6 @@ import axios from "axios";
  * Base url to make requests to the movie database
  */
 const instance = axios.create({
-    baseURL: "https://api.themoviedv.org/3",
+    baseURL: "https://api.themoviedb.org/3",
     })
 export default instance;
