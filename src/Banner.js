60fd0bbfb876d298acb5df14723e6763a9d83bf3
pericles001 <periclesadjovi@outlook.fr>
2022-06-01 17:51:29 +0100

    Fetch movie data and display banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,22 +1,42 @@
 import React, {useEffect, useState} from 'react';
+import axios from "./axios";
+import requests from "./requests";
+import {base_url} from "./Row";
 
 function Banner() {
     const [movie, setMovie] = useState([]);
 
     useEffect(() => {
-      async function fetchData(){
-          //
-      }
-      fetchData();
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length)
+                    ]
+            );
+            return request;
+        }
+        fetchData();
     }, []);
 
-
+    console.log(movie)
     return (
-        <header>
-            {/*background image*/}
-            {/*title*/}
-            {/*div with 2 buttons*/}
-            {/*description*/}
+        <header className="banner" style={{backgroundSize: "cover", backgroundImage: `url("${base_url}${movie?.backdrop_path}")`, backgroundPosition: "center center"}}>
+            <div className="banner__contents">
+                <h1>
+                    {movie?.title || movie?.name || movie?.original_name}
+                </h1>
+
+                <div className="banner_buttons">
+                    <button className="banner__button">Play</button>
+                    <button className="banner__button"> My List</button>
+                </div>
+                {/*background image*/}
+                {/*title*/}
+                {/*div with 2 buttons*/}
+                {/*description*/}
+            </div>
+"
         </header>
     );
 }
