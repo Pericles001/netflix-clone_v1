efa7933b910d44434b2d94bea0f62102c5089248
pericles001 <periclesadjovi@outlook.fr>
2022-06-01 03:47:38 +0100

    Filled requests const containers names

--- src/requests.js
+++ src/requests.js
@@ -1,1 +1,12 @@
 const API_KEY = "e955a9ffe58702222cc8fb411c451686";
+
+export default {
+    fetchTrending: ``,
+    fetchNetflixOriginals: ``,
+    fetchTopRated: ``,
+    fetchActionMovies: ``,
+    fetchHorrorMovies: ``,
+    fetchComedyMovies: ``,
+    fetchRomanceMovies: ``,
+    fetchDocumentaries: ``
+}
