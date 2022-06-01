9191127c05e7a39e831425f2fa07db4f093158d6
pericles001 <periclesadjovi@outlook.fr>
2022-06-01 15:09:05 +0100

    Remove import

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Row from "./Row";
-import Requests from "./requests";
-import './App.css';
 import requests from "./requests";
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hi clever programmer , let's build netflix clone today</h1>
-        <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
-        <Row title="Trending now" fetchUrl={requests.fetchTrending} />
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hi clever programmer , let's build netflix clone today</h1>
+            <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+            <Row title="Trending now" fetchUrl={requests.fetchTrending}/>
+        </div>
+    );
 }
 
 export default App;
