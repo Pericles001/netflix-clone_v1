b4f3dd53d6b70b591607928f7468ae25cb74cc3d
pericles001 <periclesadjovi@outlook.fr>
2022-06-01 15:08:31 +0100

    Started displaying movies informations (name, image)

--- src/Row.js
+++ src/Row.js
@@ -1,14 +1,33 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+import axios from './axios';
 
-function Row({ title }){
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({title, fetchUrl}) {
     const [movies, setMovies] = useState([]);
 
-    return (
-        <div>
-           <h2>{title}</h2>
+    //Code snippet running on conditional statement
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
 
-            { /* container -> posters */ }
+        fetchData();
+    }, [fetchUrl]);
+
+
+    return (
+        <div className='row'>
+            <h2>{title}</h2>
+            <div className='row__posters'>
+                {movies.map( movie => (
+                    <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+                ))}
+            </div>
         </div>
     )
 }
+
 export default Row;
